b40e589609f060ef98e25163cff99a84c5ceb834
amyhuyen98 <amyhuyen98@gmail.com>
2018-07-02 14:22:56 -0700

    Created user class and updated tweet class

--- app/src/main/java/com/codepath/apps/restclienttemplate/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Tweet.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
+import com.codepath.apps.restclienttemplate.models.User;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,7 +10,7 @@ public class Tweet {
     // list out the attributes
     public String body;
     public long uid; // database ID for the tweet
-    // public User user;
+    public User user;
     public String createdAt;
 
 
@@ -20,6 +22,7 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.uid = jsonObject.getLong("id");
         tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
         return tweet;
 
     }
